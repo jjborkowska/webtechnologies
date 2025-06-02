4dff6e610c545e575d635316ed5fef3c854ed2c0
jjborkowska <jjb.jjborkowska@gmail.com>
2025-06-02 18:06:16 +0200

    small changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      primary: '33f5ff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
